762db043a8f4dcff4e4ebbd7adbd7c3652e4ca0d
ocean-rey <x7flux@gmail.com>
2022-03-09 15:44:52 +0300

    check if working directory is clean

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,14 @@
-npm run test &&
-npm run compile &&
-git add lib &&
-git commit -m \"updated binaries\" &&
-git branch --show-current | 
-xargs -i git push -u origin {} &&
-npm version $type
+echo $1
+if [ -z "$(git status --porcelain)" ]; then
+    npm run test &&
+    npm run compile &&
+    git add lib &&
+    git commit -m "updated binaries" &&
+    git branch --show-current | 
+    xargs -i git push -u origin {} &&
+    npm version $1;
+    exit 0
+else
+    echo "Dirty working directory; commit or stash your uncommited changes.";
+    exit -1
+fi
